e51001389201447a93df2f831a398981a6a13268
abcxyz123susybaka <152363477+abcxyz123susybaka@users.noreply.github.com>
2025-05-27 17:34:42 +0700

    Add UI Profile for User

--- Controllers/MyAccountController.cs
+++ Controllers/MyAccountController.cs
@@ -1,11 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MovieTheater.Service;
+using MovieTheater.Services;
+using MovieTheater.ViewModels;
 
 namespace MovieTheater.Controllers
 {
     public class MyAccountController : Controller
     {
+        private readonly IAccountService _service;
+        private readonly ILogger<AccountController> _logger;
+
+        public MyAccountController(IAccountService service, ILogger<AccountController> logger)
+        {
+            _service = service;
+            _logger = logger;
+        }       
+
         [HttpGet]
-        public IActionResult MainPage(string tab = "Profile")
+        public IActionResult MainPage(string tab = "Information")
         {
             ViewData["ActiveTab"] = tab;
             return View();
@@ -18,8 +30,8 @@ namespace MovieTheater.Controllers
             {
                 case "Profile":
                     return PartialView("~/Views/Account/Tabs/Profile.cshtml");
-                case "Password":
-                    return PartialView("~/Views/Account/Tabs/Password.cshtml");
+                case "Information":
+                    return PartialView("~/Views/Account/Tabs/Information.cshtml");
                 case "Rank":
                     return PartialView("~/Views/Account/Tabs/Rank.cshtml");
                 case "Score":
@@ -32,5 +44,81 @@ namespace MovieTheater.Controllers
                     return Content("Tab not found.");
             }
         }
+
+        //public IActionResult Edit(string id)
+        //{
+        //    var account = _service.GetById(id);
+        //    if (account == null)
+        //        return NotFound();
+
+        //    var viewModel = new RegisterViewModel
+        //    {
+        //        Username = account.Account.Username,
+        //        FullName = account.Account.FullName,
+        //        DateOfBirth = (DateOnly)account.Account.DateOfBirth,
+        //        Gender = account.Account.Gender,
+        //        IdentityCard =  account .Account.IdentityCard,
+        //        Email =     account.Account.Email,
+        //        Address = account.Account.Address,
+        //        PhoneNumber = account.Account.PhoneNumber,
+        //        Image = account.Account.Image,
+        //        Password = null,
+        //        ConfirmPassword = null
+        //    };
+
+        //    return View(viewModel);
+        //}
+
+        //// POST: EmployeeController/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> EditAsync(string id, RegisterViewModel model)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return View(model);
+        //    }
+        //    try
+        //    {
+        //        if (model.ImageFile != null && model.ImageFile.Length > 0)
+        //        {
+        //            var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "image");
+        //            Directory.CreateDirectory(uploadsFolder);
+
+        //            var uniqueFileName = Guid.NewGuid().ToString() + Path.GetExtension(model.ImageFile.FileName);
+        //            var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+        //            using (var stream = new FileStream(filePath, FileMode.Create))
+        //            {
+        //                await model.ImageFile.CopyToAsync(stream);
+        //            }
+        //            model.Image = "/image/" + uniqueFileName;
+        //        }
+        //        else
+        //        {
+        //            var existingEmployee = _service.GetById(id);
+        //            if (existingEmployee != null)
+        //            {
+        //                model.Image = existingEmployee.Account.Image;
+        //            }
+        //        }
+
+        //        var success = _service.Update(id, model);
+
+        //        if (!success)
+        //        {
+        //            TempData["ErrorMessage"] = "Update failed - Username already exists";
+        //            return View(model);
+        //        }
+
+        //        TempData["ToastMessage"] = "Employee Updated Successfully!";
+        //        return RedirectToAction("MainPage", "Admin", new { tab = "EmployeeMg" });
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        TempData["ErrorMessage"] = $"Error during update: {ex.Message}";
+        //        return View(model);
+        //    }
+        //}
     }
 }
