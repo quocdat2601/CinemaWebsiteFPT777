d6404bd282622b97774777499ca361b102153700
minhnhse180474 <marcoisme0701@gmail.com>
2025-06-02 15:56:21 +0700

    promotion

--- Service/PromotionService.cs
+++ Service/PromotionService.cs
@@ -24,7 +24,10 @@ namespace MovieTheater.Service
 
         public bool Add(Promotion promotion)
         {
-
+            if (promotion == null)
+                return false;
+                
+            _promotionRepository.Add(promotion);
             return true;
         }
 
