2db6876a43aedeeb612bb45193a453ee696b3f7d
DreamFog20 <thaidat011003@gmail.com>
2025-06-09 16:18:53 +0700

    Fix UI for History

--- Controllers/ScoreController.cs
+++ Controllers/ScoreController.cs
@@ -18,7 +18,25 @@ namespace MovieTheater.Controllers
         [HttpGet]
         public IActionResult ScoreHistory()
         {
-            return View("~/Views/Account/Tabs/Score.cshtml");
+            var accountId = User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
+            if (string.IsNullOrEmpty(accountId))
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            // Lấy toàn bộ Invoice của Account_ID, mặc định là Score Adding
+            var query = _context.Invoices
+                .Where(i => i.AccountId == accountId && i.AddScore > 0);
+
+            var result = query.Select(i => new ScoreHistoryViewModel
+            {
+                DateCreated = i.BookingDate ?? DateTime.MinValue,
+                MovieName = i.MovieName ?? "N/A",
+                Score = i.AddScore ?? 0
+            }).ToList();
+
+            ViewBag.HistoryType = "add";
+            return View("~/Views/Account/Tabs/Score.cshtml", result);
         }
 
         [HttpPost]
@@ -63,31 +81,58 @@ namespace MovieTheater.Controllers
         }
 
         [HttpGet]
-        public IActionResult ScoreHistoryPartial(DateTime fromDate, DateTime toDate, string historyType)
+        public IActionResult ScoreHistoryPartial(string fromDate, string toDate, string historyType)
         {
-            var accountId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var accountId = User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
             if (string.IsNullOrEmpty(accountId))
             {
                 return Content("<div class='alert alert-danger'>Not logged in.</div>", "text/html");
             }
-            var query = _context.Invoices
-                .Where(i => i.AccountId == accountId &&
-                            i.BookingDate >= fromDate &&
-                            i.BookingDate <= toDate);
-            if (historyType == "add")
+            var query = _context.Invoices.Where(i => i.AccountId == accountId);
+
+            // Parse ngày nếu có
+            DateTime fromDateValue, toDateValue;
+            bool hasFrom = DateTime.TryParse(fromDate, out fromDateValue);
+            bool hasTo = DateTime.TryParse(toDate, out toDateValue);
+
+            if (hasFrom && hasTo)
             {
-                query = query.Where(i => i.AddScore > 0);
+                query = query.Where(i => i.BookingDate >= fromDateValue && i.BookingDate <= toDateValue);
             }
-            else if (historyType == "use")
+
+            var result = new List<ScoreHistoryViewModel>();
+            foreach (var i in query)
             {
-                query = query.Where(i => i.UseScore > 0);
+                if (i.AddScore.HasValue && i.AddScore.Value > 0)
+                {
+                    result.Add(new ScoreHistoryViewModel
+                    {
+                        DateCreated = i.BookingDate ?? DateTime.MinValue,
+                        MovieName = i.MovieName ?? "N/A",
+                        Score = i.AddScore.Value,
+                        Type = "add"
+                    });
+                }
+                if (i.UseScore.HasValue && i.UseScore.Value > 0)
+                {
+                    result.Add(new ScoreHistoryViewModel
+                    {
+                        DateCreated = i.BookingDate ?? DateTime.MinValue,
+                        MovieName = i.MovieName ?? "N/A",
+                        Score = i.UseScore.Value,
+                        Type = "use"
+                    });
+                }
             }
-            var result = query.Select(i => new ScoreHistoryViewModel
+            // Sắp xếp theo ngày giảm dần
+            result = result.OrderByDescending(x => x.DateCreated).ToList();
+
+            // Nếu filter theo historyType thì chỉ trả về đúng loại
+            if (!string.IsNullOrEmpty(historyType) && (historyType == "add" || historyType == "use"))
             {
-                DateCreated = i.BookingDate ?? DateTime.MinValue,
-                MovieName = i.MovieName ?? "N/A",
-                Score = historyType == "add" ? (i.AddScore ?? 0) : (i.UseScore ?? 0)
-            }).ToList();
+                result = result.Where(x => x.Type == historyType).ToList();
+            }
+
             return PartialView("~/Views/Account/_ScoreHistoryPartial.cshtml", result);
         }
 
--- ViewModels/ScoreHistoryViewModel.cs
+++ ViewModels/ScoreHistoryViewModel.cs
@@ -5,5 +5,6 @@
         public DateTime DateCreated { get; set; }
         public string MovieName { get; set; }
         public int Score { get; set; }
+        public string Type { get; set; }
     }
 }
