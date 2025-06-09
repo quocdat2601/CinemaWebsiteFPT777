335aa1fd312301daa2aa816b9ad00be84447d75c
DreamFog20 <thaidat011003@gmail.com>
2025-06-09 09:27:30 +0700

    Fix UI

--- Controllers/ScoreController.cs
+++ Controllers/ScoreController.cs
@@ -62,5 +62,34 @@ namespace MovieTheater.Controllers
             return View("~/Views/Account/Tabs/Score.cshtml", result);
         }
 
+        [HttpGet]
+        public IActionResult ScoreHistoryPartial(DateTime fromDate, DateTime toDate, string historyType)
+        {
+            var accountId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (string.IsNullOrEmpty(accountId))
+            {
+                return Content("<div class='alert alert-danger'>Not logged in.</div>", "text/html");
+            }
+            var query = _context.Invoices
+                .Where(i => i.AccountId == accountId &&
+                            i.BookingDate >= fromDate &&
+                            i.BookingDate <= toDate);
+            if (historyType == "add")
+            {
+                query = query.Where(i => i.AddScore > 0);
+            }
+            else if (historyType == "use")
+            {
+                query = query.Where(i => i.UseScore > 0);
+            }
+            var result = query.Select(i => new ScoreHistoryViewModel
+            {
+                DateCreated = i.BookingDate ?? DateTime.MinValue,
+                MovieName = i.MovieName ?? "N/A",
+                Score = historyType == "add" ? (i.AddScore ?? 0) : (i.UseScore ?? 0)
+            }).ToList();
+            return PartialView("~/Views/Account/_ScoreHistoryPartial.cshtml", result);
+        }
+
     }
 }
