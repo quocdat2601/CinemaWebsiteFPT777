7aa11327ebe23fd180dadcc3c49fc84e83f0da9a
Tue Do <phandogiatue51@gmail.com>
2025-07-18 12:16:13 +0700

    Fix SQL Promotion

---
+++ Models/Status.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieTheater.Models;
+
+public partial class Status
+{
+    public int StatusId { get; set; }
+
+    public string? StatusName { get; set; }
+
+    public virtual ICollection<CinemaRoom> CinemaRooms { get; set; } = new List<CinemaRoom>();
+
+    public virtual ICollection<CinemaRoom> CinemaRoomsNavigation { get; set; } = new List<CinemaRoom>();
+}
